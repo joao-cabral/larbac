6658c3586263956f5aa2cfa1988a2384086f3cd8
Joao Cabral <joaocabral1232@gmail.com>
2024-01-18 20:20:36 -0400

    feat: change components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    icon({
+      include: {
+        mdi: [
+          "github",
+          "account-file-outline",
+          "linkedin",
+          "arrow-left",
+          "file-download",
+        ],
+      },
+    }),
+  ],
+});
